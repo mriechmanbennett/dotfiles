f3117845fbd90dcb07948cc9547b9f042c6adbfe
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2023-12-05 23:53:02 -0600

    Preserve non-symlink .bak files

--- install.sh
+++ install.sh
@@ -3,8 +3,19 @@
 # Shell script to handle symlinks to the dotfiles directory
 
 InstallFile() {
-	rm ~/.$1.bak 2> /dev/null
-	mv ~/.$1 ~/.$1.bak 2> /dev/null
+	if [[ -L "~/.$1.bak" ]]
+	then
+		rm ~/.$1.bak 2> /dev/null
+		mv -n ~/.$1 ~/.$1.bak 2> /dev/null
+	else
+		if [[ -L "~/.$1" ]]
+		then
+			rm ~/.$1 2> /dev/null
+		else
+			rm ~/.$1.bak 2> /dev/null
+			mv ~/.$1 ~/.$1.bak
+		fi
+	fi
 	ln -s ${PWD}/$1 ~/.$1
 }
 
