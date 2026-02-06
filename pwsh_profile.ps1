00497b33cbcdd615a0035a7fea39f3e64e2726a5
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-02-05 22:26:23 -0800

    Remove accidental symlink, update profile to set alias for vim

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -1,1 +1,5 @@
+# Change config directory for Neovim
 $env:XDG_CONFIG_HOME = "$HOME/.config"
+
+# If Vim is not installed, alias vim for nvim
+if ( !(Test-Path -Path 'C:\Program Files\Vim') ) { New-Alias -Name vim -Value nvim }
