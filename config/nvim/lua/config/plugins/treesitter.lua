1799131ced0cd885855d2d625fc4a597c16c02d7
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-02-07 14:51:28 -0600

    tree sitter issues related to out of date arch package and I'm too lazy to fix it, disabling plugin

--- config/nvim/lua/config/plugins/treesitter.lua
+++ config/nvim/lua/config/plugins/treesitter.lua
@@ -1,7 +1,44 @@
 return {
     {
       'nvim-treesitter/nvim-treesitter',
+      enabled = false,
       lazy = false,
       build = ':TSUpdate',
+      --[[require'nvim-treesitter'.setup {
+        -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+        install_dir = vim.fn.stdpath('data') .. '/site'
+      },]]--
+      --[[
+      require'nvim-treesitter'.install {
+	  'bash',
+	  'c',
+	  'diff',
+	  'git_config',
+	  'git_rebase',
+	  'html',
+	  'ini',
+	  'javascript',
+	  'json',
+	  'jsonc',
+	  'lua',
+	  'luadoc',
+	  'luap',
+	  'make',
+	  'markdown',
+	  'markdown_inline',
+	  'powershell',
+	  'printf',
+	  'python',
+	  'query',
+	  'regex',
+	  'rust',
+	  'ssh_config',
+	  'toml',
+	  'typescript',
+	  'vim',
+	  'vimdoc',
+	  'xml',
+	  'yaml'
+      }]]--
     }
 }
