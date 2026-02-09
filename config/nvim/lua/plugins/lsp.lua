76ddab7c97c752d8509d950f015f56533c5ce8a5
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-02-09 01:11:21 -0600

    Not sure why this works but it finally does

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,20 +1,17 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-	"folke/lazydev.nvim",
-	ft = "lua", -- only load on lua files
-	opts = {
-	  library = {
-	    -- See the configuration section for more details
-	    -- Load luvit types when the `vim.uv` word is found
-	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    config = function()
+      -- Optional overrides for lua_ls (keep or expand)
+      vim.lsp.config("lua_ls", {
+	settings = {
+	  Lua = {
+	    diagnostics = {
+	      globals = { "vim" }, -- explicitly suppress "undefined global vim"
+	    },
 	  },
 	},
-      },
-    },
-    config = function()
+      })
       -- Optional: add/override settings for lua_ls
       vim.lsp.config("lua_ls", { })
       -- Start/enable the server (activates for its filetypes)
