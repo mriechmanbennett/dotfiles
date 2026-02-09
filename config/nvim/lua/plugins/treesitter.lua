7a0147afd3205ef44193805afcac954e80920f19
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-02-08 23:09:27 -0600

    Add treesitter config

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,18 @@ return {
       build = ':TSUpdate',
       config = function()
 	require("nvim-treesitter.config").setup({
-	    auto_install = true,
-	    highlight = { enable = true },
+	    auto_install = false,
+	    sync_install = false,
+	    highlight = { 
+		enable = true,
+                disable = function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+			return true
+		     end
+		 end,
+	    },
 	    indent = { enable = true },
       	    ensure_installed = {
       	        'bash',
