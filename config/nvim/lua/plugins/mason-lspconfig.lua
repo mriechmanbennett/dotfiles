6ac4070aebefa57a29c915e1ff8ffa9c228a09c1
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-02-09 16:18:48 -0600

    Commit lspconfig, mason, and mason-lspconfig that seems to be working on Linux

---
+++ config/nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+	"lua_ls",
+	"pyright",
+	"bashls",
+	"yamlls",
+	"jsonls",
+	"markdown_oxide",
+	"texlab",
+	"docker_compose_language_service",
+	"terraformls",
+	"ansiblels",
+	"csharp_ls",
+	"html",
+	"powershell_es"
+      }
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  }
+}
