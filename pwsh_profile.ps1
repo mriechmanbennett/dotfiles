29b2d78ee30e81665f286ccadfdadfef060a76b8
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-02-15 12:42:44 -0600

    Add powershell aliases

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -1,6 +1,5 @@
 # Opt out of telemetry
-# Set it two ways because I can't be bothered to learn which is right
-$env:POWERSHELL_TELEMETRY_OPTOUT=1
+# Set it two ways because I can't be bothered to learn which is right $env:POWERSHELL_TELEMETRY_OPTOUT=1
 $POWERSHELL_TELEMETRY_OPTOUT=1
 
 # Change config directory for Neovim
@@ -14,8 +13,23 @@ if ( !(Test-Path -Path 'C:\Program Files\Vim') ) { New-Alias -Name vim -Value nv
 #########
 
 # Shortcuts
-New-Alias -Name c -Value clear
+New-Alias -Name c -Value Clear-Host
 New-Alias -Name ap -Value ansible-playbook
 New-Alias -Name tf -Value terraform
 New-Alias -Name wo -Value Write-Output
+
+# git shortcuts
+New-Alias -Name g -Value 'git'
+New-Alias -Name ga -Value 'git add ./'
+New-Alias -Name gco -Value 'git co'
+New-Alias -Name gcom -Value 'git commit'
+New-Alias -Name gcomm -Value 'git commit -m'
+New-Alias -Name gst -Value 'git st'
+New-Alias -Name gp -Value 'git push'
 New-Alias -Name loga -Value 'git loga'
+New-Alias -Name commit -Value 'git commit'
+
+# Mistake corrections
+New-Alias -Name ':wq' -Value 'exit'
+New-Alias -Name ':q' -Value 'exit'
+New-Alias -Name 'ivm' -Value 'vim'
