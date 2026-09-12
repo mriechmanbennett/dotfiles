01ee7cf0379c3783e78bb5c6053af75955b5f295
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-09-12 11:30:57 -0500

    Correct nvim alias creation logic

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -7,8 +7,8 @@ $POWERSHELL_TELEMETRY_OPTOUT=1
 # Change config directory for Neovim
 $env:XDG_CONFIG_HOME = "$HOME/.config"
 
-# If Vim is not installed, alias vim for nvim
-if ( !(Test-Path -Path 'C:\Program Files\Vim') ) { New-Alias -Name vim -Value nvim }
+# If Nvim is installed, create alias
+if ( Test-Path -Path 'C:\Program Files\Neovim' ) { New-Alias -Name vim -Value nvim }
 
 #########
 # Aliases
