348e7c4f380c54d62d48c166c7fb8e957323f833
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2026-09-12 12:13:21 -0500

    Update Install-Rsat.ps1

--- windows/install/rsat/Install-Rsat.ps1
+++ windows/install/rsat/Install-Rsat.ps1
@@ -1,4 +1,4 @@
-$CapabilitiesToAdd =
+$CapabilitiesShouldBePresent =
     "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0",
     "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0",
     "Rsat.Dns.Tools~~~~0.0.1.0",
@@ -6,6 +6,10 @@ $CapabilitiesToAdd =
     "Rsat.BitLocker.Recovery.Tools~~~~0.0.1.0",
     "Rsat.CertificateServices.Tools~~~~0.0.1.0"
 
-Foreach ($Capability in $CapabilitiesToAdd) {
+$CapabilitiesToInstall = Foreach ( $Capability in $CapabilitiesShouldBePresent ) {
+    Get-WindowsCapability -Online -Name $Capability | Where-Object State -ne 'Installed'
+}
+
+Foreach ( $Capability in $CapabilitiesToInstall ) {
     Start-Process -NoNewWindow -PassThru -FilePath powershell.exe -ArgumentList "Add-WindowsCapability -Online -ErrorAction SilentlyContinue -Name $Capability"
 }
